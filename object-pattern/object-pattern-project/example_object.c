86e8f3e9f2c001fbfe3fcf434474ad297fe2749d
Henrik Samuelsson <henrik.samuelsson@gmail.com>
2023-06-10 18:40:54 +0200

    Add small application using the object pattern.

--- object-pattern/object-pattern-project/example_object.c
+++ object-pattern/object-pattern-project/example_object.c
@@ -4,6 +4,6 @@
 
 int example_object_init(struct example_object *self)
 {
-    memset(self, 0, sizeof(*self))
+    memset(self, 0, sizeof(*self));
     return 0;
 }
--- object-pattern/object-pattern-project/main.c
+++ object-pattern/object-pattern-project/main.c
@@ -1,8 +1,19 @@
+#include "example_object.h"
+
 #include <stdio.h>
 #include <stdlib.h>
+
 
 int main(void)
 {
     printf("Hello Object Pattern");
+    printf("\n");
+
+    struct example_object object;
+    example_object_init(&object);
+
+    printf("Our object data = %d", object.data);
+    printf("\n");
+
     return EXIT_SUCCESS;
 }
