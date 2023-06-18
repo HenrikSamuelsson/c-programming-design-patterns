4a7688da8bc6c5c0dcc3467f1860c7fd3e0374ef
Henrik Samuelsson <henrik.samuelsson@gmail.com>
2023-06-18 12:37:23 +0200

    Add new set of object functions.

--- object-pattern/object-pattern-project/vehicle.c
+++ object-pattern/object-pattern-project/vehicle.c
@@ -1,9 +1,31 @@
-#include "example_object.h"
+#include "vehicle.h"
 
 #include <string.h>
 
-int animal_init(struct animal *self)
+int vehicle_init(struct vehicle *self, const char *name)
 {
-    memset(self, 0, sizeof(*self));
+    strcpy(self->name, name);
+    self->speed = 0;
+    return 0;
+}
+
+int vehicle_deinit(struct vehicle * self)
+{
+    // Nothing to do here.
+    return 0;
+}
+
+int vehicle_accelerate(struct vehicle *self)
+{
+    self->speed += 1;
+}
+
+int vehicle_decelerate(struct vehicle *self)
+{
+    self->speed -= 1;
+    if (self->speed < 0)
+    {
+        self->speed = 0;
+    }
     return 0;
 }
--- object-pattern/object-pattern-project/vehicle.h
+++ object-pattern/object-pattern-project/vehicle.h
@@ -1,10 +1,13 @@
 #include <stdint.h>
 
-struct animal
+struct vehicle
 {
-    char *name;
-    uint32_t weight;
-    uint32_t age;
+    char name[32];
+    int32_t speed;
 };
 
-int animal_init(struct animal *self);
+int vehicle_init(struct vehicle *self, const char *name);
+int vehicle_deinit(struct vehicle *self);
+
+int vehicle_accelerate(struct vehicle *self);
+int vehicle_decelerate(struct vehicle *self);
